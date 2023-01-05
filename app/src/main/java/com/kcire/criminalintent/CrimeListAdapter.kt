1a191f512d349c13973171a7730550384879d0e4
Erick Ochieng <erickochieng766@gmail.com>
2023-01-05 17:13:57 +0300

    RecyclerView, Adapter, ViewHolder, ListItem

---
+++ app/src/main/java/com/kcire/criminalintent/CrimeListAdapter.kt
@@ -0,0 +1,38 @@
+package com.kcire.criminalintent
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.kcire.criminalintent.databinding.ListItemCrimeBinding
+
+class CrimeListAdapter(
+    private val crimes: List<Crime>
+) : RecyclerView.Adapter<CrimeHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CrimeHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemCrimeBinding.inflate(inflater, parent, false)
+        return CrimeHolder(binding)
+    }
+
+    override fun onBindViewHolder(
+        holder: CrimeHolder,
+        position: Int
+    ) {
+        val crime = crimes[position]
+        holder.apply {
+            binding.crimeTitle.text = crime.title
+            binding.crimeDate.text = crime.date.toString()
+        }
+    }
+
+    override fun getItemCount() = crimes.size
+}
+
+class CrimeHolder(
+    val binding: ListItemCrimeBinding
+) : RecyclerView.ViewHolder(binding.root) {
+
+}
